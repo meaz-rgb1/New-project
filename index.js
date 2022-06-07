a25299e3f244513c274967cc78a0fd3f0267c62f
Your Namemeaz-rgb.meaza <you@example.commeaz-rgb.meazateklu01@gmail.com>
2022-06-07 16:08:41 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+
+       document.querySelector('#alert').
+       addEventListener ('click', function () => (alert
+       ('I like Red Panda')
+
+       function alertMe (){
+           return alert  ('I like Red Panda')
+       }
+           e.preventDefault()
+           let animalObj= {
+               name:e.target.name.value,
+               imageUrl:e.target.image_Url.value,
+               description.target.description.value,
+           }
+                   
+ function getRed-Panda (animalObj){
+     fetch('"https://i.imgur.com/VI6Vh88.png",')
+     .then(resv=> resv.json())
+     .then(animalData => animalDatea.forEach(animal=>renderOneAnimal(animal)))
+         console.log (Red panda.db)
+ }
+      
